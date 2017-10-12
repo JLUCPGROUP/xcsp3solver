b602cc1742dfcaf0c91783709937b13938d9a37f
李哲 <leezear@live.cn>
2017-10-12 21:07:16 +0800

    12334

--- solver/OTSolver/test.cpp
+++ solver/OTSolver/test.cpp
@@ -1,34 +1,161 @@
 #pragma once
 #include <sstream>
-#include <fstream>
 #include <string>
+
+#include "ortools/base/join.h"
 #include "XCSP3PrintCallbacks.h"
 #include "BMFileParser.h"
 #include "Timer.h"
-#include "ortools/constraint_solver/constraint_solveri.h"
+#include "ortools/constraint_solver/constraint_solver.h"
 #include "ortools/base/logging.h"
 #include "ortools/util/tuple_set.h"
-#include <complex>
-//#include <windows.h>
+#include <glog/logging.h>
+
 using namespace cudacp;
 using namespace std;
 using namespace operations_research;
+
 //#define LOGFILE
 
 const string XPath = "BMPath.xml";
 
-class MiddleVariableIndexSelector :public Solver::IndexEvaluator1 {
-public:
-	MiddleVariableIndexSelector(const int64 n) :n_(n), mid_((n - 1) / 2) {}
-	~MiddleVariableIndexSelector() {}
-	int64 Run(int64 index) const
-	{
-		return abs(mid_ - n_);
-	}
-private:
-	const int64 n_;
-	const int64 mid_;
+////typedef ResultCallback1<int64, int64> IndexEvaluator1;
+//
+//class MiddleVariableIndexSelector : public Solver::IndexEvaluator1 {
+//public:
+//	MiddleVariableIndexSelector(const int64 n) : n_(n), middle_var_index_((n - 1) / 2) {}
+//	~MiddleVariableIndexSelector() {}
+//	int64 Run(const int64 index) {
+//		return abs(middle_var_index_ - index);
+//	}
+//
+//private:
+//	const int64 n_;
+//	const int64 middle_var_index_;
+//};
+
+enum FindSolution {
+	Finding = 0,
+	FindOne = 1,
+	Non = -1
 };
+
+namespace {
+
+class NQueensDecisionBuilder : public DecisionBuilder {
+public:
+	NQueensDecisionBuilder(const int size, const vector<IntVar*>& vars) :
+		size_(size), vars_(vars), middle_var_index_((size - 1) / 2) {
+		CHECK_EQ(vars_.size(), size_);
+	}
+	~NQueensDecisionBuilder() {}
+
+	// Select a variable with the smallest domain starting from the center
+	IntVar* SelectVar(Solver* const s) const {
+		IntVar* selected_var = nullptr;
+		int64 id = -1;
+		int64 min_domain_size = kint64max;
+
+		// go left on the chessboard
+		for (int64 i = middle_var_index_; i >= 0; --i) {
+			IntVar* const var = vars_[i];
+			if (!var->Bound() && var->Size() < min_domain_size) {
+				selected_var = var;
+				id = i;
+				min_domain_size = var->Size();
+			}
+		}
+
+		// go right on the chessboard
+		for (int64 i = middle_var_index_ + 1; i < size_; ++i) {
+			IntVar* const var = vars_[i];
+			if (!var->Bound() && var->Size() < min_domain_size) {
+				selected_var = var;
+				id = i;
+				min_domain_size = var->Size();
+			}
+		}
+
+		if (id == -1) {
+			return nullptr;
+		}
+		else {
+			return selected_var;
+		}
+	}
+
+	int64 count_number_of_row_incompatibilities(const int64 row) const {
+		int64 count = 0;
+		for (int64 i = 0; i < size_; ++i) {
+			if (!vars_[i]->Contains(row)) {
+				++count;
+			}
+		}
+		return count;
+	}
+
+	//  For a given variable, take the row with the least compatible columns, starting from the center
+	int64 SelectValue(const IntVar* const v) const {
+		CHECK_GE(v->Size(), 2);
+
+		int64 max_incompatible_columns = -1;
+		int64 selected_value = -1;
+
+		const int64 vmin = v->Min();
+		const int64 vmax = v->Max();
+		const int64 v_middle = (vmin + vmax) / 2;
+
+		for (int64 i = v_middle; i >= vmin; --i) {
+			if (v->Contains(i)) {
+				const int64 nbr_of_incompatibilities = count_number_of_row_incompatibilities(i);
+				if (nbr_of_incompatibilities > max_incompatible_columns) {
+					max_incompatible_columns = nbr_of_incompatibilities;
+					selected_value = i;
+
+				}
+			}
+		}
+
+		for (int64 i = v_middle; i <= vmin; ++i) {
+			if (v->Contains(i)) {
+				const int64 nbr_of_incompatibilities = count_number_of_row_incompatibilities(i);
+				if (nbr_of_incompatibilities > max_incompatible_columns) {
+					max_incompatible_columns = nbr_of_incompatibilities;
+					selected_value = i;
+
+				}
+			}
+		}
+
+		CHECK_NE(selected_value, -1);
+
+		return selected_value;
+	}
+
+	Decision* Next(Solver* const s) override {
+		IntVar* const var = SelectVar(s);
+		if (nullptr != var) {
+			const int64 value = SelectValue(var);
+			return s->MakeAssignVariableValue(var, value);
+		}
+
+		return nullptr;
+	}
+
+private:
+	const int size_;
+	const vector<IntVar*> vars_;
+	const int middle_var_index_;
+}; //  class NQueensDecisionBuilder
+
+
+
+}  // namespace
+
+DecisionBuilder* MakeNQueensDecisionBuilder(Solver* const s, const int size, const vector<IntVar*>& vars) {
+	return s->RevAlloc(new NQueensDecisionBuilder(size, vars));
+}
+
 
 int main() {
 	string bm_path;
@@ -56,21 +183,35 @@ int main() {
 	}
 	cout << "-------------------------solve-------------------------" << endl;
 	//vector<SearchMonitor*> monitors;
-	//MiddleVariableIndexSelector *misel = new MiddleVariableIndexSelector(s);
+	//SolutionCollector* const collector = s.MakeFirstSolutionCollector();
+	//collector->Add(vars);
+	//monitors.push_back(collector);
+	//vector<SearchMonitor*> monitors;
+	//DecisionBuilder* const db = MakeNQueensDecisionBuilder(&s, vars.size(), vars);
+	//SolutionCollector* const solution_counter = s.MakeAllSolutionCollector(NULL);
+	//monitors.push_back(solution_counter);
+	//MiddleVariableIndexSelector * index_evaluator = new MiddleVariableIndexSelector(vars.size());
+	//DecisionBuilder* const db = s.MakePhase(vars, Solver::CHOOSE_MIN_SIZE, Solver::ASSIGN_MIN_VALUE);
+	//DecisionBuilder* const db = s.MakePhase(vars, index_evaluator, Solver::ASSIGN_CENTER_VALUE); //  ASSIGN_CENTER_VALUE, ASSIGN_MIN_VALUE
 	DecisionBuilder* const db = s.MakePhase(vars, Solver::CHOOSE_MIN_SIZE, Solver::ASSIGN_MIN_VALUE);
-	s.NewSearch(db);
-	Timer t;
-	//s.AddPropagationMonitor()
-	if (s.NextSolution())
-		for (auto v : vars) {
+	SearchLimit* limit = s.MakeTimeLimit(2000);
+	s.NewSearch(db, limit);
+	int64 solve_time = 0;
+	FindSolution find = Finding;
+	if (s.NextSolution()) {
+		find = FindOne;
+		solve_time = s.wall_time();
+		for (auto v : vars)
 			cout << v->name() << " = " << v->Value() << " ";
-		}
-
+		cout << endl;
+	}
+	else {
+		find = Non;
+	}
 	s.EndSearch();
-	cout << endl;
-	const int64_t solve_time = t.elapsed();
+
+	cout << "solve time = " << s.wall_time() << endl;
 	delete hm;
-	cout << "solve time = " << solve_time << endl;
 	cout << "--------------------------end--------------------------" << endl;
 	return 0;
-}
+};
--- solver/OTTest/test.cpp
+++ solver/OTTest/test.cpp
@@ -1,89 +1,212 @@
+// Copyright 2011-2014 Google
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// //Copyright 2010-2014 Google
-// //Licensed under the Apache License, Version 2.0 (the "License");
-// //you may not use this file except in compliance with the License.
-// //You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// //    http://www.apache.org/licenses/LICENSE-2.0
-//
-// //Unless required by applicable law or agreed to in writing, software
-// //distributed under the License is distributed on an "AS IS" BASIS,
-// //WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// //See the License for the specific language governing permissions and
-// //limitations under the License.
-//
-//
-// //N-queens problem
-//
-// // unique solutions: http://www.research.att.com/~njas/sequences/A000170
-// // distinct solutions: http://www.research.att.com/~njas/sequences/A002562
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 //
-//#include "ortools/base/commandlineflags.h"
-//#include "ortools/base/integral_types.h"
-//#include "ortools/base/logging.h"
-//#include "ortools/base/stringprintf.h"
-//#include "ortools/constraint_solver/constraint_solver.h"
+// n-Queens Problem
 //
-//DEFINE_int32(
-//	size, 88,
-//	"Size of the problem. If equal to 0, will test several increasing sizes.");
-//
-//namespace operations_research {
-//void NQueens(int size) {
-//	CHECK_GE(size, 1);
-//	Solver s("nqueens");
-//
-//	// model
-//	std::vector<IntVar*> queens;
-//	for (int i = 0; i < size; ++i) {
-//		queens.push_back(s.MakeIntVar(0, size - 1, StringPrintf("queen%04d", i)));
-//	}
-//	for (int i = 0; i < size - 1; ++i) {
-//		for (int j = i + 1; j < size; ++j) {
-//			s.AddConstraint(s.MakeNonEquality(queens[i], queens[j]));
-//			s.AddConstraint(s.MakeNonEquality(s.MakeSum(queens[i], i), queens[j]));
-//			s.AddConstraint(s.MakeNonEquality(s.MakeSum(queens[i], -i), queens[j]));
-//		}
-//	}
-//	std::vector<SearchMonitor*> monitors;
-//	DecisionBuilder* const db = s.MakePhase(queens, Solver::CHOOSE_FIRST_UNBOUND,
-//		Solver::ASSIGN_MIN_VALUE);
-//	monitors.push_back(s.MakeSearchLog(1000000));
-//	s.Solve(db, monitors);  // go!
-//}
-//}  // namespace operations_research
-//
-//int main(int argc, char** argv) {
-//	gflags::ParseCommandLineFlags(&argc, &argv, true);
-//	operations_research::NQueens(FLAGS_size);
-//	return 0;
-//}
+// Use of customized search strategies with the help of customized DecisionBuilder.
 
-#include <bitset>
-#include <iostream>
-#include <vector>
-using namespace std;
+#include "ortools/base/join.h"
+#include "ortools/base/commandlineflags.h"
+#include "ortools/base/logging.h"
+#include "ortools/base/stringprintf.h"
+#include "ortools/constraint_solver/constraint_solver.h"
 
-template<class T>
-class BitSetModel {
+#include "nqueens_utilities.h"
+
+DEFINE_int32(size, 4, "Size of the problem.");
+DEFINE_int64(time_limit, 0, "Time limit on the solving"
+	" process. 0 means no time limit.");
+DECLARE_bool(use_symmetry);
+
+namespace operations_research {
+
+namespace {
+
+class NQueensDecisionBuilder : public DecisionBuilder {
 public:
-	BitSetModel() {};
-	virtual ~BitSetModel() {};
-	//vector<vector<vector<bitset<N>>>> bsd;
-	//vector<bitset<N>> bd;
-	T xixi;
-	//int mds = N;
-	//int vs_size;
-protected:
-};
+	NQueensDecisionBuilder(const int size, const std::vector<IntVar*>& vars) :
+		size_(size), vars_(vars), middle_var_index_((size - 1) / 2) {
+		CHECK_EQ(vars_.size(), size_);
+	}
+	~NQueensDecisionBuilder() {}
 
-int main() {
-	bitset<64> bt;
-	bt[12] = 1;
-	int n = 12;
+	// Select a variable with the smallest domain starting from the center
+	IntVar* SelectVar(Solver* const s) {
+		IntVar* selected_var = nullptr;
+		int64 id = -1;
+		int64 min_domain_size = kint64max;
 
-	BitSetModel<> bsm();
-	cout << bt << endl;
+		// go left on the chessboard
+		for (int64 i = middle_var_index_; i >= 0; --i) {
+			IntVar* const var = vars_[i];
+			if (!var->Bound() && var->Size() < min_domain_size) {
+				selected_var = var;
+				id = i;
+				min_domain_size = var->Size();
+			}
+		}
 
+		// go right on the chessboard
+		for (int64 i = middle_var_index_ + 1; i < size_; ++i) {
+			IntVar* const var = vars_[i];
+			if (!var->Bound() && var->Size() < min_domain_size) {
+				selected_var = var;
+				id = i;
+				min_domain_size = var->Size();
+			}
+		}
+
+		if (id == -1) {
+			return nullptr;
+		}
+		else {
+			return selected_var;
+		}
+	}
+
+	int64 count_number_of_row_incompatibilities(int64 row) {
+		int64 count = 0;
+		for (int64 i = 0; i < size_; ++i) {
+			if (!vars_[i]->Contains(row)) {
+				++count;
+			}
+		}
+		return count;
+	}
+
+	//  For a given variable, take the row with the least compatible columns, starting from the center
+	int64 SelectValue(const IntVar* const v) {
+		CHECK_GE(v->Size(), 2);
+
+		int64 max_incompatible_columns = -1;
+		int64 selected_value = -1;
+
+		const int64 vmin = v->Min();
+		const int64 vmax = v->Max();
+		const int64 v_middle = (vmin + vmax) / 2;
+
+		for (int64 i = v_middle; i >= vmin; --i) {
+			if (v->Contains(i)) {
+				const int64 nbr_of_incompatibilities = count_number_of_row_incompatibilities(i);
+				if (nbr_of_incompatibilities > max_incompatible_columns) {
+					max_incompatible_columns = nbr_of_incompatibilities;
+					selected_value = i;
+
+				}
+			}
+		}
+
+		for (int64 i = v_middle; i <= vmin; ++i) {
+			if (v->Contains(i)) {
+				const int64 nbr_of_incompatibilities = count_number_of_row_incompatibilities(i);
+				if (nbr_of_incompatibilities > max_incompatible_columns) {
+					max_incompatible_columns = nbr_of_incompatibilities;
+					selected_value = i;
+
+				}
+			}
+		}
+
+		CHECK_NE(selected_value, -1);
+
+		return selected_value;
+	}
+
+	Decision* Next(Solver* const s) {
+		IntVar* const var = SelectVar(s);
+		if (nullptr != var) {
+			const int64 value = SelectValue(var);
+			return s->MakeAssignVariableValue(var, value);
+		}
+
+		return nullptr;
+	}
+
+private:
+	const int size_;
+	const std::vector<IntVar*> vars_;
+	const int middle_var_index_;
+}; //  class NQueensDecisionBuilder
+
+
+
+}  // namespace
+
+DecisionBuilder* MakeNQueensDecisionBuilder(Solver* const s,
+	const int size,
+	const std::vector<IntVar*>& vars) {
+	return s->RevAlloc(new NQueensDecisionBuilder(size, vars));
+}
+
+
+void NQueens(int size) {
+	CHECK_GE(size, 1);
+	Solver s("nqueens");
+
+	// model
+	std::vector<IntVar*> queens;
+	for (int i = 0; i < size; ++i) {
+		queens.push_back(s.MakeIntVar(0, size - 1, StringPrintf("x%04d", i)));
+	}
+	s.AddConstraint(s.MakeAllDifferent(queens));
+
+	std::vector<IntVar*> vars(size);
+	for (int i = 0; i < size; ++i) {
+		vars[i] = s.MakeSum(queens[i], i)->Var();
+	}
+	s.AddConstraint(s.MakeAllDifferent(vars));
+	for (int i = 0; i < size; ++i) {
+		vars[i] = s.MakeSum(queens[i], -i)->Var();
+	}
+	s.AddConstraint(s.MakeAllDifferent(vars));
+
+	std::vector<SearchMonitor*> monitors;
+
+	SolutionCollector* const solution_counter = s.MakeAllSolutionCollector(NULL);
+	monitors.push_back(solution_counter);
+	SolutionCollector* const collector = s.MakeFirstSolutionCollector();
+	collector->Add(queens);
+	monitors.push_back(collector);
+
+	DecisionBuilder* const db = MakeNQueensDecisionBuilder(&s, size, queens);
+
+	s.Solve(db, monitors);  // go!
+	CheckNumberOfSolutions(size, solution_counter->solution_count());
+
+	const int num_solutions = solution_counter->solution_count();
+	const int64 time = s.wall_time();
+
+	std::cout << "============================" << std::endl;
+	std::cout << "size: " << size << std::endl;
+	std::cout << "The Solve method took "
+		<< time / 1000.0 << " seconds" << std::endl;
+	std::cout << "Number of solutions: " << num_solutions << std::endl;
+	std::cout << "Failures: " << s.failures() << std::endl;
+	std::cout << "Branches: " << s.branches() << std::endl;
+	std::cout << "Backtracks: " << s.fail_stamp() << std::endl;
+	std::cout << "Stamps: " << s.stamp() << std::endl;
+	PrintFirstSolution(size, queens, collector);
+}
+}   // namespace operations_research
+
+int main(int argc, char **argv) {
+	google::ParseCommandLineFlags(&argc, &argv, true);
+	if (FLAGS_use_symmetry) {
+		LOG(FATAL) << "Symmetries not yet implemented!";
+	}
+
+	operations_research::NQueens(FLAGS_size);
+
+	return 0;
 }
